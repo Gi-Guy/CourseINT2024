a0c70a007028d94670b6af1b98a09af7c22e9704
Guy <Guy9502@Gmail.com>
2025-01-22 19:57:04 +0200

    fixing

--- objects/dist/employees.js
+++ objects/dist/employees.js
@@ -1,4 +1,4 @@
-function getEmployees() {
+function getEmployee() {
     var name = prompt("Enter employee name:");
     var age = parseInt(prompt("Enter employee age:") || "0");
     var department = prompt("Enter employee department (optional):");
@@ -14,7 +14,7 @@ function getEmployees() {
 function start() {
     var employees = [];
     for (var i = 0; i < 5; i++) {
-        employees.push(getEmployees());
+        employees.push(getEmployee());
     }
     alert(employees.map(function (employee) { return "Name: " + employee.name + ", Age: " + employee.age + ", Department: " + (employee.department || 'N/A') + ", Salary: " + employee.sallary; }).join('\n'));
 }
--- objects/employees.ts
+++ objects/employees.ts
@@ -5,7 +5,7 @@ type Employee = {
   sallary: number;
 };
 
-function getEmployees(){
+function getEmployee(): Employee{
     const name = prompt("Enter employee name:");
     const age = parseInt(prompt("Enter employee age:") || "0");
     const department = prompt("Enter employee department (optional):");
@@ -24,8 +24,9 @@ function getEmployees(){
 function start(){
     const employees: Employee[] = [];
     for(let i = 0; i < 5; i++){
-        employees.push(getEmployees());
+        employees.push(getEmployee());
     }
     alert(employees.map(employee => `Name: ${employee.name}, Age: ${employee.age}, Department: ${employee.department || 'N/A'}, Salary: ${employee.sallary}`).join('\n'));
 }
+
 start();
